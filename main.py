7b15a2dab0f4c152103faf02989a3e27bb2612fe
Anna-Franziska <aism.studentsqgmail.com>
2022-04-04 22:54:16 +0200

    Übungsblatt 3

--- main.py
+++ main.py
@@ -1,25 +1,49 @@
+#Exercise 1
+def count_vowels(text):
+    vowels = "aeiou"
+    text = text.casefold()
+    count = {}.fromkeys(vowels, 0)
+    for i in text:
+        if i in count:
+            count[i] += 1
+            return count
+    else:
+        return 42
+
+#Exercise 3
+class Vehicle:
+    def __init__(self, color, fuel_type):
+        self.color = color
+        self.fuel_type = fuel_type
+        __call__ = self
 
 
-
-def remove_duplicates(lst):
-    dupli = set(lst)
-    for x in lst:
-        return dupli
-
-# I tried out the function number 4
-practice = [3, 44, 5, 6, 44]
-# print(remove_duplicates(practice))
+class Car(Vehicle):
+    def __init__(self, color, fuel_type, doors):
+        super().__init__(color, fuel_type)
+        self.doors = doors
+    def __str__(self):
+        return print("Color:", self.color, "Fuel Type:", self.fuel_type, "Doors:", self.doors)
 
 
+class Bus(Vehicle):
+    def __init__(self, color, fuel_type, passengers):
+        super(). __init__ (color, fuel_type)
+        self.passengers = passengers
+    def __str__(self):
+        return print("Color:", self.color, "Fuel Type:", self.fuel_type, "Passengers", self.passengers)
 
-def list_func(numbers, integer):
-   for i in range(len(numbers)):
-     if  numbers[i] == integer:
-          numbers[i] = 6
-   print(numbers[::-1])
-   return numbers
+#car1 = Car("red", "gas", "four") - practise
 
-#trying out code again
-cucumber = [4, 5, 5, 7, 5]
-print(list_func(cucumber, 5))
+#Exercise 4
+class Book:
+    def __init__(self, name, author):
+        self.name = name
+        self.author = author
+    def __str__(self):
+            return print(self.name, ",", self.author)
+
+
+#book1 = Book("Dune", "Franke Herbert") - more practise
+
 
